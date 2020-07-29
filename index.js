e3adb177fefddff064abcffd49661df163a2e5af
Luis717 <hack.jlom@gmail.com>
2020-07-29 01:23:30 -0600

    checking password

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hola David Fernando Rivera, te estoy vigilando!');
+    res.send('Hello, World!');
 });
 
 app.post('/post', function (req, res) {
@@ -11,5 +11,5 @@ app.post('/post', function (req, res) {
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000! v0.1.1');
-    console.log(process.env);
+    console.log(process.env.password);
 });
