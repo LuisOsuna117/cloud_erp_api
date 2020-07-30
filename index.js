3ba095f67f0ed05c0a5b96fce3b599ba9bbc6abe
Luis717 <hack.jlom@gmail.com>
2020-07-29 23:46:06 -0600

    Functional POST

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const log = require("./config/chalk");
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+function getResponse(status, relsoved) {
+    return response = {
+        status: status,
+        response: resolved
+    }
+}
 /* Database configuration */
 const pool = require("./config/database");
 
@@ -10,14 +21,27 @@ app.get('/', function (req, res) {
 
 app.post('/post', function (req, res) {
     pool.getConnection(function (err, connection) {
-        if (err) throw err; // not connected!
+        if (err) {
+            log.red(err);
+            if (err.code === "PROTOCOL_CONNECTION_LOST") {
+                log.red("Database connection was closed.");
+            }
+            if (err.code === "ER_CON_COUNT_ERROR") {
+                log.red("Database has too many connections.");
+            }
+            if (err.code === "ECONNREFUSED") {
+                log.red("Database connection was refused.");
+            }
+        }
         // Use the connection
         connection.query(`SELECT * FROM ${req.body.table}`, function (error, results, fields) {
-            res.send(results);
+            res.send(getResponse(res.status,results));
             // When done with the connection, release it.
             connection.release();
             // Handle error after the release.
-            if (error) throw error;
+            if (error) {
+                log.red(`MySQLERR ${error.code}: ${error.message}`) 
+            }
             // Don't use the connection here, it has been returned to the pool.
         });
     });
