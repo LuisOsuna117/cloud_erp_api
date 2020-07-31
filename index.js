035fec552fa4573b83f8f97a37c2133b58bd061b
Luis717 <hack.jlom@gmail.com>
2020-07-31 01:47:21 -0600

    Put method added

--- index.js
+++ index.js
@@ -47,6 +47,34 @@ app.post('/post', function (req, res) {
     });
 });
 
+app.put('/put', function (req, res) {
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            log.red(err);
+            if (err.code === "PROTOCOL_CONNECTION_LOST") {
+                log.red("Database connection was closed.");
+            }
+            if (err.code === "ER_CON_COUNT_ERROR") {
+                log.red("Database has too many connections.");
+            }
+            if (err.code === "ECONNREFUSED") {
+                log.red("Database connection was refused.");
+            }
+        }
+        // Use the connection
+        connection.query(`SELECT * FROM ${req.body.table}`, function (error, results, fields) {
+            res.send(getResponse(res.status, results));
+            // When done with the connection, release it.
+            connection.release();
+            // Handle error after the release.
+            if (error) {
+                log.red(`MySQLERR ${error.code}: ${error.message}`)
+            }
+            // Don't use the connection here, it has been returned to the pool.
+        });
+    });
+});
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
