a32757315a5d92bc2fb9a3b286c963fd65e110e3
Luis717 <hack.jlom@gmail.com>
2020-07-31 19:27:39 -0600

    Get inventory

--- index.js
+++ index.js
@@ -6,17 +6,35 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-function getResponse(status, resolved) {
-    return response = {
-        status: status,
-        response: resolved
-    }
-}
 /* Database configuration */
 const pool = require("./config/database");
 
-app.get('/', function (req, res) {
-    res.send('Hello, World!');
+app.get('/getInventory', function (req, res) {
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            log.red(err);
+            if (err.code === "PROTOCOL_CONNECTION_LOST") {
+                log.red("Database connection was closed.");
+            }
+            if (err.code === "ER_CON_COUNT_ERROR") {
+                log.red("Database has too many connections.");
+            }
+            if (err.code === "ECONNREFUSED") {
+                log.red("Database connection was refused.");
+            }
+        }
+        // Use the connection
+        connection.query(`CALL getAllInventoryProducts`, function (error, results, fields) {
+            res.send(results);
+            // When done with the connection, release it.
+            connection.release();
+            // Handle error after the release.
+            if (error) {
+                log.red(`MySQLERR ${error.code}: ${error.message}`)
+            }
+            // Don't use the connection here, it has been returned to the pool.
+        });
+    });
 });
 
 app.post('/post', function (req, res) {
