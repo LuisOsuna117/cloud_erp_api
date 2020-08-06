a58b19bd250519c96d485fadbf6e1de70d46f882
Luis717 <hack.jlom@gmail.com>
2020-08-05 18:18:31 -0600

    SQL Script update

--- database/database.sql
+++ database/database.sql
@@ -22,9 +22,6 @@ DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
   `userid` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(10) DEFAULT "",
-  `password` varchar(10) DEFAULT "",
-  `usertype` varchar(1) NOT NULL,
   `fname` varchar(20) NOT NULL,
   `lname` varchar(20) NOT NULL,
   `email` varchar(40) NOT NULL,
@@ -34,6 +31,34 @@ CREATE TABLE `users` (
   `city` varchar(15) NOT NULL,
   `salary` double DEFAULT 0 
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `admins`(
+  `adminid` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `userid` int NOT NULL,
+  `username` varchar(10) DEFAULT "",
+  `password` varchar(10) DEFAULT "",
+  FOREIGN KEY (`userid`) REFERENCES `users`(`userid`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `businesspremises` (
+  `bpid` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `adminid` int NOT NULL,
+  `bpname` varchar(20) NOT NULL,
+  FOREIGN KEY (`adminid`) REFERENCES `admins`(`adminid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/* inventory table structure */
+
+DROP TABLE IF EXISTS `inventory`;
+
+CREATE TABLE `inventory` (
+  `productid` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `bpid` int NOT NULL,
+  `pname` varchar(20) NOT NULL,
+  `pquantity` varchar(50) NOT NULL,
+  `plastpurchase` date NOT NULL,
+  FOREIGN KEY (`bpid`) REFERENCES `businesspremises`(`bpid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /* clients table structure */
 
@@ -73,42 +98,33 @@ CREATE TABLE `purchases` (
 
 CREATE TABLE `purchasesdesc` (
   `purchaseid` int NOT NULL,
-  `pname` varchar(50) NOT NULL,
+  `productid` int NOT NULL,
   `pprice` double NOT NULL,
   `pquantity` date NOT NULL,
-  FOREIGN KEY (`purchaseid`) REFERENCES `purchases`(`purchaseid`)
+  FOREIGN KEY (`purchaseid`) REFERENCES `purchases`(`purchaseid`),
+  FOREIGN KEY (`productid`) REFERENCES `inventory`(`productid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /* sales table structure */
 
 CREATE TABLE `sales` (
   `saleid` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `sname` varchar(50) NOT NULL,
+  `sname` varchar(20) NOT NULL,
   `sclient` int NOT NULL,
+  `sdesc` varchar(50) NOT NULL,
   `stotal` double NOT NULL,
   `pdate` date NOT NULL,
   FOREIGN KEY (`sclient`) REFERENCES `clients`(`clientid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/* salesdesc table structure */
+/* usedmaterial table structure */
 
-CREATE TABLE `salesdesc` (
+CREATE TABLE `usedmaterial` (
   `saleid` int NOT NULL,
-  `sname` varchar(50) NOT NULL,
-  `sprice` double NOT NULL,
-  `squantity` date NOT NULL,
-  FOREIGN KEY (`saleid`) REFERENCES `sales`(`saleid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-/* inventory table structure */
-
-DROP TABLE IF EXISTS `inventory`;
-
-CREATE TABLE `inventory` (
-  `producid` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `pname` varchar(20) NOT NULL,
-  `pquantity` varchar(50) NOT NULL,
-  `plastpurchase` date NOT NULL
+  `productid` int NOT NULL,
+  `mquantity` int NOT NULL,
+  FOREIGN KEY (`saleid`) REFERENCES `sales`(`saleid`),
+  FOREIGN KEY (`productid`) REFERENCES `inventory`(`productid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
