efa67701a824273ff43ca43d3ff60da71210c143
Luis717 <hack.jlom@gmail.com>
2020-08-10 23:52:39 -0600

    Test

--- index.js
+++ index.js
@@ -30,11 +30,39 @@ app.get('/getPurchases', function (req, res) {
         }
         // Use the connection
         connection.query(`CALL loadPurchases`, function (error, results, fields) {
-            
-            var temp = 0;
+            var temp = 1;
+            var products = [];
             var helper = results[0];
             for (var k in helper) {
-                console.log(k, helper[k].purchaseid);
+                if (temp != helper[k].purchaseid) {
+                    if (temp == 0) {
+                        temp = helper[k].purchaseid;
+                        result.temp = {
+                            "purchaseid": helper[k].purchaseid,
+                            "sname": helper[k].sname,
+                            "ptotal": helper[k].ptotal,
+                            "pdate": helper[k].pdate,
+                        }
+                    } else {
+                        result.temp.products = products;
+                        temp = helper[k].purchaseid;
+                        result.temp = {
+                            "purchaseid": helper[k].purchaseid,
+                            "sname": helper[k].sname,
+                            "ptotal": helper[k].ptotal,
+                            "pdate": helper[k].pdate,
+                        }
+                    }
+                    
+                } else {
+                    var tmprod = {
+                        "name": helper[k].pname,
+                        "pquantity": helper[k].pquantity,
+                        "pprice": helper[k].pprice,
+                        "plastpurchase": helper[k].plastpurchase
+                    }
+                    products.push(temprod);
+                }
             }
             res.send(result);
             // When done with the connection, release it.
@@ -52,7 +80,7 @@ app.post('/userLogin', function (req, res) {
     pool.getConnection(function (err, connection) {
         if (err) {
             manageError(err);
-        } 
+        }
         // Use the connection
         connection.query(`CALL userLogin('${req.body.username}','${req.body.password}')`, function (error, results, fields) {
             res.send(results);
@@ -60,7 +88,7 @@ app.post('/userLogin', function (req, res) {
             connection.release();
             // Handle error after the release.
             if (error) {
-                log.red(`MySQLERR ${error.code}: ${error.message}`) 
+                log.red(`MySQLERR ${error.code}: ${error.message}`)
             }
             // Don't use the connection here, it has been returned to the pool.
         });
@@ -85,6 +113,8 @@ app.put('/put', function (req, res) {
         });
     });
 });
+
+
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
